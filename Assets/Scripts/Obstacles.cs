4956e3e64e32df680a5554d87b079c3737d2d4a2
Siddhant Sharma <sid.sharma0904@gmail.com>
2026-09-26 20:47:55 +0530

    .

--- Assets/Scripts/Obstacles.cs
+++ Assets/Scripts/Obstacles.cs
@@ -2,9 +2,12 @@ using UnityEngine;
 
 public class Obstacles : MonoBehaviour
 {
+    public GameObject ExplosionEffect;
+
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Player")
         gameObject.SetActive(false);
+        Instantiate(ExplosionEffect, transform.position,Quaternion.identity);
     }
 }
